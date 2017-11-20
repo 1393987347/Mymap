a3f7dcd1317af49a9c9a6e14802e8e9c4914d6d3
1393987347 <1393987347@qq.com>
2017-11-20 13:29:01 +0800

    mymap

---
+++ mymap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+template <typename T>
+class Mymap
+{
+    Mymap <string, string> map = new Mymap<string, string>();  
+};
+
+void insert(string, string)
+{ 
+
+}
+
+  
+
+int main(void)
+{
+
+}
